d2f96920bebd2bbc1a75a87b35c48229474bc49c
Linas Mickevicius <linasmickevicius@Unknown-6c-40-08-8f-2f-c2.home>
2016-03-24 23:59:54 +0000

    improved register validation

--- registerScript.php
+++ registerScript.php
@@ -7,50 +7,68 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
   $email = $_POST['email'];
   $password = $_POST['password'];
 
-  //remove all illegal characters from input
-  $username = filter_var($username, FILTER_SANITIZE_STRING);
-  $email = filter_var($email, FILTER_SANITIZE_EMAIL);
-  $password = filter_var($password, FILTER_SANITIZE_STRING);
+  // $username = htmlspecialchars($username, ENT_QUOTES);
 
-  //check if input is valid
-  if(filter_var($email, FILTER_VALIDATE_EMAIL) === false){
-    $_SESSION['status'] = 'Email is not valid';
-    header('location: register.php');
-    return;
-  }
+  if(isset($username) && isset($email) && isset($password)){
 
-  $check_username = $conn->prepare("SELECT username FROM user WHERE username = ?");
-  $check_username->bindParam(1,$username, PDO::PARAM_STR);
-  $check_username->fetch();
-  $check_username->execute();
-  $check_username->closeCursor();
+    if(!filter_var($username, FILTER_SANITIZE_STRING) === false){
+      if(!filter_var($email, FILTER_SANITIZE_EMAIL) === false){
+        if(!filter_var($password, FILTER_SANITIZE_STRING) === false){
+          if(!filter_var($email, FILTER_VALIDATE_EMAIL) === false){
 
-  if($check_username->rowCount()==1){
-      $_SESSION['status'] = 'Username is already in use';
-      header('location: register.php');
-  }else{
-      $check_email = $conn->prepare("SELECT email FROM user WHERE email = ?");
-      $check_email->bindParam(1,$email, PDO::PARAM_STR);
-      $check_email->fetch();
-      $check_email->execute();
-      $check_email->closeCursor();
+            $check_username = $conn->prepare("SELECT username FROM user WHERE username = ?");
+            $check_username->bindParam(1,$username, PDO::PARAM_STR);
+            $check_username->fetch();
+            $check_username->execute();
+            $check_username->closeCursor();
 
-      if($check_email->rowCount()==1){
-          $_SESSION['status'] = 'Email is already in use';
+            if($check_username->rowCount()==1){
+                $_SESSION['status'] = 'Username is already in use';
+                header('location: register.php');
+            }else{
+                $check_email = $conn->prepare("SELECT email FROM user WHERE email = ?");
+                $check_email->bindParam(1,$email, PDO::PARAM_STR);
+                $check_email->fetch();
+                $check_email->execute();
+                $check_email->closeCursor();
+
+                if($check_email->rowCount()==1){
+                    $_SESSION['status'] = 'Email is already in use';
+                    header('location: register.php');
+                }else{
+                  $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+                  $register = $conn->prepare("INSERT INTO user(username,email,password) VALUES(?,?,?)");
+                  $register->bindParam(1,$username, PDO::PARAM_STR);
+                  $register->bindParam(2,$email, PDO::PARAM_STR);
+                  $register->bindParam(3,$hashed_password, PDO::PARAM_STR);
+                  $register->execute();
+                  $register->closeCursor();
+
+                  $_SESSION['status'] = 'Registered';
+                  header('location: register.php');
+                }
+            }
+
+        }else{
+          $_SESSION['status'] = 'Email is not valid';
           header('location: register.php');
+        }
       }else{
-        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-        $register = $conn->prepare("INSERT INTO user(username,email,password) VALUES(?,?,?)");
-        $register->bindParam(1,$username, PDO::PARAM_STR);
-        $register->bindParam(2,$email, PDO::PARAM_STR);
-        $register->bindParam(3,$hashed_password, PDO::PARAM_STR);
-        $register->execute();
-        $register->closeCursor();
-
-        $_SESSION['status'] = 'Registered';
+        $_SESSION['status'] = 'Invalid username';
         header('location: register.php');
       }
+    }else{
+      $_SESSION['status'] = 'Invalid email';
+      header('location: register.php');
+    }
+  }else{
+    $_SESSION['status'] = 'Invalid password';
+    header('location: register.php');
   }
+
+}
+  $_SESSION['status'] = 'Only numbers and letters allowed';
+  header('location: register.php');
 }
 ?>
