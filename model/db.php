380af5d7466d457997002d943a7aeabc09537384
Linas Mickevicius <linasmickevicius@Unknown-6c-40-08-8f-2f-c2.home>
2016-04-04 21:50:27 +0100

    minor changes

--- model/db.php
+++ model/db.php
@@ -6,74 +6,77 @@ $db_options = array(
 , PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC      // fetch associative arrays (default: mixed arrays)
 );
 
-$conn = new PDO('mysql:host=127.0.0.1;dbname=todo;charset=utf8', 'root', '', $db_options);    // important! specify the character encoding in the DSN string, don't use SET NAMES
+$conn = new PDO('mysql:host=127.0.0.1;dbname=todo;charset=utf8', 'root', '', $db_options);
 
 function open($sess_path, $sess_name){
   GLOBAL $db_options;
   // echo '<p>session open fired</p>';
-  $conn = new PDO('mysql:host=127.0.0.1;dbname=todo;charset=utf8', 'root', '', $db_options);    // important! specify the character encoding in the DSN string, don't use SET NAMES
-$sessionId =  session_id();
-  $sql = "INSERT INTO session SET session_id =" . $conn->quote($sessionId) . ", session_data = '' ON DUPLICATE KEY UPDATE session_lastaccesstime = NOW()";
-  $conn->query($sql);
+  $conn = new PDO('mysql:host=127.0.0.1;dbname=todo;charset=utf8', 'root', '', $db_options);
+  $sess_id =  session_id();
+  $initialData = '';
+
+  $setupSession = $conn->prepare("INSERT INTO session(id, data) VALUES(?,?) ON DUPLICATE KEY UPDATE lastaccessed = NOW()");
+  $setupSession->bindParam(1, $sess_id, PDO::PARAM_STR);
+  $setupSession->bindParam(2, $initialData, PDO::PARAM_STR);
+  $setupSession->execute();
 }
 
 
-function read($sessionId){
-  GLOBAL $db_options;
-  // echo '<p>session read fired</p>';
-  $conn = new PDO('mysql:host=127.0.0.1;dbname=todo;charset=utf8', 'root', '', $db_options);    // important! specify the character encoding in the DSN string, don't use SET NAMES
+function read($sess_id){
+   GLOBAL $db_options;
+   // echo '<p>session read fired</p>';
+   $conn = new PDO('mysql:host=127.0.0.1;dbname=todo;charset=utf8', 'root', '', $db_options);
 
-  $sql = "SELECT session_data FROM session where session_id =" . $conn->quote($sessionId);
-     $result = $conn->query($sql);
-     $data = $result->fetchColumn();
-     $result->closeCursor();
-
-     return $data;
-
+   $readSession = $conn->prepare("SELECT data FROM session where id = ? ");
+   $readSession->bindParam(1, $sess_id, PDO::PARAM_STR);
+   $readSession->execute();
+   $data = $readSession->fetchColumn();
+   $readSession->closeCursor();
+   return $data;
 }
 
 
-function write($sessionId, $data){
+function write($sess_id, $data){
   GLOBAL $db_options;
   // echo '<p>session write fired</p>';
-  $conn = new PDO('mysql:host=127.0.0.1;dbname=todo;charset=utf8', 'root', '', $db_options);    // important! specify the character encoding in the DSN string, don't use SET NAMES
+  $conn = new PDO('mysql:host=127.0.0.1;dbname=todo;charset=utf8', 'root', '', $db_options);
 
-  $sql = "INSERT INTO session SET session_id =" . $conn->quote($sessionId) . ", session_data =" . $conn->quote($data) . " ON DUPLICATE KEY UPDATE session_data =" . $conn->quote($data);
-  $conn->query($sql);
-
+  $writeToSession = $conn->prepare("INSERT INTO session(id, data) VALUES(?,?) ON DUPLICATE KEY UPDATE data = ?");
+  $writeToSession->bindParam(1, $sess_id, PDO::PARAM_STR);
+  $writeToSession->bindParam(2, $data, PDO::PARAM_STR);
+  $writeToSession->bindParam(3, $data, PDO::PARAM_STR);
+  $writeToSession->execute();
 }
 
 
 function close() {
   GLOBAL $db_options;
   // echo '<p>session close fired</p>';
-    $sessionId = session_id();
-    //perform some action here
+  $sess_id = session_id();
 }
 
 
-function destroy($sessionId) {
+function destroy($sess_id) {
   GLOBAL $db_options;
-  $conn = new PDO('mysql:host=127.0.0.1;dbname=todo;charset=utf8', 'root', '', $db_options);    // important! specify the character encoding in the DSN string, don't use SET NAMES
+  $conn = new PDO('mysql:host=127.0.0.1;dbname=todo;charset=utf8', 'root', '', $db_options);
 
-    $sql = "DELETE FROM session WHERE session_id =" . $conn->quote($sessionId);
-    $conn->query($sql);
+  $sessionCleanUp = $conn->prepare("DELETE FROM session WHERE id = ?");
+  $sessionCleanUp->bindParam(1, $sess_id, PDO::PARAM_STR);
+  $sessionCleanUp->execute();
 
-    setcookie(session_name(), "", time() - 9999);
+  setcookie(session_name(), "", time() - 9999);
 }
 
 
-function gc($lifetime) {
+function gc($sessionLifetime) {
   GLOBAL $db_options;
-  $conn = new PDO('mysql:host=127.0.0.1;dbname=todo;charset=utf8', 'root', '', $db_options);    // important! specify the character encoding in the DSN string, don't use SET NAMES
+  $conn = new PDO('mysql:host=127.0.0.1;dbname=todo;charset=utf8', 'root', '', $db_options);
 
-  $sql = "DELETE FROM session WHERE session_lastaccesstime < DATE_SUB(NOW(), INTERVAL " . $lifetime . " SECOND)";
-  $conn->query($sql);
+  $sessionCleanUp = $conn->prepare("DELETE FROM session WHERE lastaccessed < DATE_SUB(NOW(), INTERVAL ? SECOND)");
+  $sessionCleanUp->bindParam(1, $sess_id, PDO::PARAM_STR);
+  $sessionCleanUp->bindParam(2, $sessionLifetime, PDO::PARAM_STR);
+  $sessionCleanUp->execute();
 }
-
-
-
-
 
 
 session_set_save_handler("open", "close", "read", "write", "destroy", "gc");
